c20ebe026c4c7fe3ab0689d6e8a1fa6b9c8f9a3b
veman1 <371635581@qq.com>
2018-03-30 17:59:12 +0800

    增加首页

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,15 @@
+import index_config from 'index_config'
+
 Page({
   data: {
-    cityName: '佛山'
-  }
+    cityName: '佛山',
+    ccurrent: 0,
+    config: index_config
+  },
+  tapCompanyCategory(e) {
+    this.setData({
+      ccurrent: e.currentTarget.dataset.current
+    })
+  },
 })
+
---
+++ pages/index/index_config.js
@@ -0,0 +1,22 @@
+export default {
+  nav_list: [
+    {
+      img: '/images/index_nav_free.png',
+      text: '三免',
+      url: '',
+      open_type: 'navigate'
+    },
+    {
+      img: '/images/index_nav_gift.png',
+      text: '三送',
+      url: '',
+      open_type: 'navigate'
+    },
+    {
+      img: '/images/index_nav_safe.png',
+      text: '三保',
+      url: '',
+      open_type: 'navigate'
+    }
+  ]
+}
