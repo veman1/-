d84da34c8d84585d29984f7d4a13ebc5ac9a3bd2
veman1 <371635581@qq.com>
2018-03-31 17:15:52 +0800

    案例列表页静态

---
+++ pages/case_index/case_index_config.js
@@ -0,0 +1,29 @@
+export default {
+  scrollbar_list: [
+    { text: '酒店', id: '0' },
+    { text: '幼儿园', id: '1' },
+    { text: '影院', id: '2' },
+    { text: '办公', id: '3' },
+    { text: '餐饮', id: '4' },
+    { text: '酒店', id: '0' },
+    { text: '酒店', id: '0' },
+    { text: '酒店', id: '0' },
+  ],
+  style_list: [
+    { text: '宜家', id: '1' },
+    { text: '现代', id: '2' },
+    { text: '简欧', id: '3' },
+    { text: '欧式', id: '4' },
+    { text: '北欧', id: '5' },
+    { text: '地中海', id: '6' },
+  ],
+  house_type_list: [
+    { text: '户型1', id: '0' },
+    { text: '户型2', id: '1' },
+    { text: '户型3', id: '2' },
+    { text: '户型4', id: '3' },
+    { text: '户型5', id: '4' },
+    { text: '户型6', id: '5' },
+    { text: '户型7', id: '6' },
+  ],
+}
